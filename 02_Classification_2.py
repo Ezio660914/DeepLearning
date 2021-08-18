73316a9a1d41b098ccfb24feb4d00ea0e3a09ef0
Ezio660914 <424176345@qq.com>
2021-08-18 20:04:41 +0800

    Update 02_Classification_2.py
    change the learning rate;
    split the dataset;
    plot the loss curve;

--- 02_Classification_2.py
+++ 02_Classification_2.py
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.datasets import make_circles
+from sklearn.model_selection import train_test_split
 
 
 def PlotDecisionBoundary(model, X, y):
@@ -45,12 +46,13 @@ def PlotDecisionBoundary(model, X, y):
     plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.get_cmap('RdYlBu'))
     plt.xlim(xv.min(), xv.max())
     plt.ylim(yv.min(), yv.max())
-    plt.show()
+    # plt.show()
 
 
 def main():
     samplesAmount = 1000
     X, y = make_circles(samplesAmount, noise=0.03, random_state=13)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=26)
     circles = pd.DataFrame({'X0': X[:, 0],
                             'X1': X[:, 1],
                             'label:': y})
@@ -62,11 +64,20 @@ def main():
         tf.keras.layers.Dense(10, tf.keras.activations.relu),
         tf.keras.layers.Dense(1, tf.keras.activations.sigmoid),
     ])
-    model.compile(tf.keras.optimizers.Adam(),
+    model.compile(tf.keras.optimizers.Adam(learning_rate=0.01),
                   tf.keras.losses.BinaryCrossentropy(),
                   [tf.keras.metrics.BinaryAccuracy()])
-    model.fit(X, y, epochs=100)
-    PlotDecisionBoundary(model, X, y)
+    history = model.fit(X_train, y_train, epochs=20)
+    model.evaluate(X_test, y_test)
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title('Train')
+    PlotDecisionBoundary(model, X_train, y_train)
+    plt.subplot(1, 2, 2)
+    plt.title('Test')
+    PlotDecisionBoundary(model, X_test, y_test)
+    pd.DataFrame(history.history).plot()
+    plt.show()
     pass
 
 
