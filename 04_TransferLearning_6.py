6b74334c49a0cd8d924ac67e6f0b8334c2cf11f5
Ezio660914 <424176345@qq.com>
2021-08-26 15:15:06 +0800

    first train

--- 04_TransferLearning_6.py
+++ 04_TransferLearning_6.py
@@ -33,6 +33,7 @@ testDir = "resource/food101/101_food_classes_10_percent/test/"
 logDir = "./log/tensorflow_hub"
 saveModelDir = "./savedModel/food101_101Classes_1"
 preTrainedModelDir = "./savedModel/efficientnet_b0_feature-vector_1"
+checkpointDir = saveModelDir + f"/checkpoint_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}"
 imgShape = (224, 224, 3)
 imgSize = (224, 224)
 initialEpochs = 5
@@ -48,7 +49,46 @@ def main():
                                                                 batch_size=32,
                                                                 image_size=imgSize,
                                                                 shuffle=False)
-    
+    # create checkpoint callback
+    ckptCallback = keras.callbacks.ModelCheckpoint(checkpointDir,
+                                                   monitor="val_accuracy",
+                                                   save_weights_only=True,
+                                                   save_best_only=True)
+    # setup data augmentation
+    dataAugmentation = keras.Sequential([
+        RandomFlip("horizontal"),
+        RandomRotation(0.2),
+        RandomHeight(0.2),
+        RandomWidth(0.2),
+        RandomZoom(0.2),
+        # Rescaling(1 / 255.)
+    ], name="DataAugmentation")
+
+    # create model
+    baseModel = keras.applications.EfficientNetB0(include_top=False)
+    baseModel.trainable = False
+
+    # setup model architecture with trainable top layers
+    inputs = keras.layers.Input(shape=imgShape,
+                                name="InputLayer")
+    x = dataAugmentation(inputs)
+    x = baseModel(x, training=False)
+    x = keras.layers.GlobalAveragePooling2D(name="GlobalAveragePoolLayer")(x)
+    outputs = keras.layers.Dense(len(trainData.class_names),
+                                 activation="softmax",
+                                 name="OutputLayer")(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(keras.optimizers.Adam(),
+                  keras.losses.CategoricalCrossentropy(),
+                  ["accuracy"])
+    model.summary()
+    history = model.fit(trainData,
+                        epochs=initialEpochs,
+                        steps_per_epoch=len(trainData),
+                        validation_data=testData,
+                        validation_steps=int(0.15 * len(testData)),
+                        callbacks=[ckptCallback])
+
     pass
 
 
