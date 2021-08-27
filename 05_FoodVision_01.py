23d52c5ee187449ea39bbfa41621e889cbdfbb34
Ezio660914 <424176345@qq.com>
2021-08-27 16:58:26 +0800

    build the model

--- 05_FoodVision_01.py
+++ 05_FoodVision_01.py
@@ -21,6 +21,7 @@ import matplotlib.image as mpimg
 import pandas as pd
 import pathlib
 import datetime
+from sklearn.preprocessing import OneHotEncoder
 
 try:
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -37,46 +38,101 @@ preTrainedModelDir = "./savedModel/efficientnet_b0_feature-vector_1"
 checkpointDir = saveModelDir + f"/checkpoint_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}"
 imgShape = (224, 224, 3)
 imgSize = (224, 224)
-initialEpochs = 5
+initialEpochs = 3
+numClasses = len(os.listdir(trainDir))
 
 
-def PreProcessImg(imagePath, imgSize=(224, 224)):
+def PreProcessImg(imagePath, label, imgSize=(224, 224)):
     """
     Converts image datatype from uint8 to float32,
     re-range from 255 to 1, and reshapes image to the given imgSize
     """
     file = tf.io.read_file(imagePath)
-    image = tf.image.decode_image(file, channels=3, expand_animations=False)
+    image = tf.image.decode_jpeg(file, 3)
+    image = tf.cast(image, tf.float32)
     image = tf.image.resize(image, imgSize)
-    image = image / 255.0
-    return image
+    return image, label
 
 
-def GetDataPathList(dataDir):
+def GetDataList(dataDir):
+    """
+    get all data path from data directory
+    """
     dataPathList = list(pathlib.Path(dataDir).glob("*/*"))
-    return [str(path) for path in dataPathList]
+    pathStrList = [str(path) for path in dataPathList]
+    labelStrList = np.array([path.parent.name for path in dataPathList])[:, None]
+    labelOneHot = OneHotEncoder(dtype=np.float32).fit_transform(labelStrList).toarray()
+    return pathStrList, labelOneHot
 
 
-def GetLabelList(dataDir):
-    from sklearn.preprocessing import OneHotEncoder
-    dataPathList = list(pathlib.Path(dataDir).glob("*/*"))
-    classNames = np.array([path.parent.name for path in dataPathList])[:, np.newaxis]
-    classNamesOneHot = OneHotEncoder().fit_transform(classNames).toarray().astype(np.int64)
-    return classNamesOneHot
+def GetDataset(dir, shuffle=True):
+    # obtain the dataset
+    pathStrList, labelOneHot = GetDataList(dir)
+    dataPathDs = tf.data.Dataset.from_tensor_slices((pathStrList, labelOneHot))
+    # load and preprocess the image as dataset
+    dataset = dataPathDs.map(map_func=PreProcessImg, num_parallel_calls=tf.data.AUTOTUNE)
+    # shuffle data and turn it into batches and prefetch it (load it faster)
+    if shuffle:
+        prefetchData = dataset.shuffle(buffer_size=1000).batch(batch_size=32).prefetch(
+            buffer_size=tf.data.AUTOTUNE)
+    else:
+        prefetchData = dataset.batch(batch_size=32).prefetch(
+            buffer_size=tf.data.AUTOTUNE)
+    return prefetchData
 
 
 def main():
-    # obtain the train dataset
-    trainPathDs = tf.data.Dataset.from_tensor_slices(GetDataPathList(trainDir))
-    # load and preprocess the train image as dataset
-    trainImgDs = trainPathDs.map(map_func=PreProcessImg, num_parallel_calls=tf.data.AUTOTUNE)
-    # load the labels
-    trainLabelDs = tf.data.Dataset.from_tensor_slices(GetLabelList(trainDir))
-    # zip as (image,label) dataset
-    trainImgLabelDs = tf.data.Dataset.zip((trainImgDs, trainLabelDs))
-    # shuffle train data and turn it into batches and prefetch it (load it faster)
-    trainData = trainImgLabelDs.shuffle(buffer_size=1000).batch(batch_size=32).prefetch(buffer_size=tf.data.AUTOTUNE)
-    
+    # obtain train dataset
+    trainData = GetDataset(trainDir)
+    # trainData = keras.preprocessing.image_dataset_from_directory(trainDir,
+    #                                                              label_mode="categorical",
+    #                                                              batch_size=32,
+    #                                                              image_size=imgSize,
+    #                                                              shuffle=True)
+    # obtain test dataset
+    testData = GetDataset(testDir, False)
+    # testData = keras.preprocessing.image_dataset_from_directory(testDir,
+    #                                                             label_mode="categorical",
+    #                                                             batch_size=32,
+    #                                                             image_size=imgSize,
+    #                                                             shuffle=False)
+    print([i for i in testData.take(1)])
+    # print([i for i in testData.take(1)])
+    # create model callbacks
+    ckptCallback = keras.callbacks.ModelCheckpoint(checkpointDir,
+                                                   monitor="val_accuracy",
+                                                   save_best_only=True,
+                                                   save_weights_only=True,
+                                                   verbose=1)
+    # setup mixed precision training
+    keras.mixed_precision.set_global_policy("mixed_float16")
+    # build feature extraction model
+    baseModel = keras.applications.EfficientNetB0(include_top=False)
+    baseModel.trainable = False
+    # create functional model
+    inputs = keras.layers.Input(imgShape)
+    x = baseModel(inputs, training=False)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    x = keras.layers.Dense(numClasses)(x)
+    outputs = keras.layers.Activation("softmax", dtype=tf.float32)(x)
+    model = keras.Model(inputs, outputs)
+    # compile the model
+    model.compile(keras.optimizers.Adam(),
+                  keras.losses.CategoricalCrossentropy(),
+                  ["accuracy"])
+    # model.summary()
+    # for layer in model.layers:
+    #     print((layer.name, layer.trainable, layer.dtype, layer.dtype_policy))
+
+    # fit the model
+    history = model.fit(trainData,
+                        epochs=initialEpochs,
+                        steps_per_epoch=int(0.1 * len(trainData)),
+                        validation_data=testData,
+                        validation_steps=int(0.15 * len(testData)),
+                        callbacks=[ckptCallback])
+    model.evaluate(testData)
+
     pass
 
 
