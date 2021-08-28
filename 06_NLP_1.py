51c0b3e749f0f969bfe952d0e0ae327403c15d57
Ezio660914 <424176345@qq.com>
2021-08-28 17:21:14 +0800

    become one with the data

--- 06_NLP_1.py
+++ 06_NLP_1.py
@@ -13,13 +13,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # or any {'0', '1', '2'}
 import tensorflow as tf
 import tensorflow.keras as keras
 import tensorflow_hub as hub
-from tensorflow.keras.layers.experimental.preprocessing import *
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import pandas as pd
 import pathlib
 import datetime
+from sklearn.model_selection import train_test_split
 
 try:
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -28,7 +26,30 @@ except:
     print("GPU error")
 
 
+class StaticConst:
+    dataDir = pathlib.Path("resource/nlp_getting_started")
+    trainDF = pd.read_csv(dataDir / "train.csv")
+    testDF = pd.read_csv(dataDir / "test.csv")
+    trainDFShuffled = trainDF.sample(frac=1)
+
+
+def GetDataRandomly(dataFrame: pd.DataFrame, amount=1):
+    randomIndex = random.randint(0, len(dataFrame) - amount)
+    randomDF = dataFrame.sample(frac=1)
+    for row in randomDF[["text", "target"]][randomIndex:randomIndex + amount].itertuples():
+        _, text, target = row
+        print("---------------------------------\n")
+        print(f"Target: {target}", "(real disaster)" if target > 0 else "(not real disaster)")
+        print(f"Text:\n{text}\n")
+
+
 def main():
+    GetDataRandomly(StaticConst.trainDF)
+    # use train test split to split training data into training and validation sets
+    trainData, valData, trainLabel, valLabel = train_test_split(StaticConst.trainDFShuffled["text"].to_numpy(),
+                                                                StaticConst.trainDFShuffled["target"].to_numpy(),
+                                                                test_size=0.1)
+    
     pass
 
 
