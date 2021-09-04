e7ca887646a78807c410cefa07ed54ea65efaf30
Ezio660914 <424176345@qq.com>
2021-09-04 22:02:26 +0800

    make predictions

--- 08_TimeSeries_5.py
+++ 08_TimeSeries_5.py
@@ -37,6 +37,8 @@ batchSize = 1024
 nNeurons = 512
 nLayers = 4
 nStacks = 30
+intoFuture = 14
+trainModel = False
 
 
 def BuildModel(windowSize, horizon):
@@ -48,6 +50,50 @@ def BuildModel(windowSize, horizon):
     return model
 
 
+def MakeFutureForecasts(values, model: keras.Model, into_future, window_size):
+    """make future forecasts into_future steps after values ends
+    returns future forecasts as a list of float values"""
+    futureForecast = []
+    lastWindow = values[-window_size:]
+    # make into_future number of predictions, altering the data which gets predicted on each
+    for i in range(into_future):
+        # predict on the last window then append it again
+        futurePred = model.predict(tf.expand_dims(lastWindow, 0))
+        print(f"Predicting on:\n{lastWindow} -> Prediction: {tf.squeeze(futurePred).numpy()}\n")
+        futureForecast.append(tf.squeeze(futurePred).numpy())
+        lastWindow = np.append(lastWindow, futurePred)[-window_size:]
+    return futureForecast
+
+
+def GetFutureDates(start_date, into_future, offset=1):
+    """return array of datetime values ranging from start_date to start_date + into_future"""
+    start_date = start_date + np.timedelta64(offset, "D")
+    end_date = start_date + np.timedelta64(into_future, "D")
+    return np.arange(start_date, end_date, dtype="datetime64[D]")
+
+
+def plot_time_series(timesteps, values, format='.', start=0, end=None, label=None):
+    """
+    Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
+
+    Parameters
+    ---------
+    timesteps : array of timesteps
+    values : array of values across time
+    format : style of plot, default "."
+    start : where to start the plot (setting a value will index from start of timesteps & values)
+    end : where to end the plot (setting a value will index from end of timesteps & values)
+    label : label to show on plot of values
+    """
+    # Plot the series
+    plt.plot(timesteps[start:end], values[start:end], format, label=label)
+    plt.xlabel("Time")
+    plt.ylabel("BTC Price")
+    if label:
+        plt.legend(fontsize=14)  # make label bigger
+    plt.grid(True)
+
+
 def main():
     # create N-Beats data inputs
     df = pd.read_csv(dataDir,
@@ -55,11 +101,11 @@ def main():
                      index_col=["Date"])
     # add in block reward feature to data frame
     priceDf = pd.DataFrame(df["Closing Price (USD)"]).rename(columns={"Closing Price (USD)": "Price"})
-    bitcoinPricesNbeats = priceDf.copy()
+    bitcoinPricesShifted = priceDf.copy()
     for i in range(windowSize):
-        bitcoinPricesNbeats[f"Price_{i + 1}"] = bitcoinPricesNbeats["Price"].shift(i + 1)
-    X = pd.DataFrame(bitcoinPricesNbeats.dropna().drop("Price", axis=1), dtype=np.float32)
-    y = pd.DataFrame(bitcoinPricesNbeats.dropna()["Price"], dtype=np.float32)
+        bitcoinPricesShifted[f"Price_{i + 1}"] = bitcoinPricesShifted["Price"].shift(i + 1)
+    X = pd.DataFrame(bitcoinPricesShifted.dropna().drop("Price", axis=1), dtype=np.float32)
+    y = pd.DataFrame(bitcoinPricesShifted.dropna()["Price"], dtype=np.float32)
 
     # turn data into datasets
     featuresDataset = tf.data.Dataset.from_tensor_slices(X)
@@ -77,12 +123,23 @@ def main():
                                                verbose=1,
                                                save_best_only=True,
                                                save_weights_only=True)
-    history = model.fit(allData,
-                        epochs=epochs,
-                        callbacks=[callback])
+    if trainModel:
+        history = model.fit(allData,
+                            epochs=epochs,
+                            callbacks=[callback])
+        pd.DataFrame(history.history["loss"]).plot()
+        plt.show()
     model.load_weights(checkpointDir)
     model.evaluate(allData)
-    pd.DataFrame(history.history["loss"]).plot()
+    futureForecast = MakeFutureForecasts(y, model, intoFuture, windowSize)
+    futureForecast = np.insert(futureForecast, 0, y["Price"][-1])
+    print(futureForecast)
+    nextTimeSteps = GetFutureDates(X.index[-1], intoFuture)
+    nextTimeSteps = np.insert(nextTimeSteps, 0, X.index[-1])
+    print(nextTimeSteps)
+    plt.figure(figsize=(10, 7))
+    plot_time_series(priceDf.index, priceDf, start=2400, format="-", label="Acture")
+    plot_time_series(nextTimeSteps, futureForecast, format="-", label="Predicted")
     plt.show()
     exit(0)
 
